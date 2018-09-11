735cd13891c6630eb4dcc094c5be618eaa91ef30
Nicholas Werner <micropacman@gmail.com>
2018-09-11 21:22:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,8 @@ function deleteFromObjectByKey(object, key) {
   delete newObj[key]
   return newObj
 }
+
+function destructivelyDeleteFromObject(object, key) {
+  delete object[key]
+  return object
+}
